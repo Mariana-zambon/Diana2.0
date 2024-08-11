418c049898d22e8b863a7deb6e7e1722c805405b
Mariana-zambon <88859896+Mariana-zambon@users.noreply.github.com>
2024-08-11 19:28:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,106 @@
+export const perguntas = [
+    
+    {
+        enunciado: "Durante a Juventude, Diana se depara com essas duas escolhas, qual é a mais prudente de se seguir? ",
+        alternativas: [
+            {
+                texto:"Diana deve investir em sua educação. Ela economiza dinheiro de trabalhos de meio período e bolsas de estudo para pagar a faculdade sem contrair muitas dívidas.",
+                afirmacao: [" Na juventude Diana fez cursos e construiu um currículo exemplar.",
+                            "Na juventude, Diana eonomizou sabiamente e evitou dívidas.",
+                            "Durante a juventude, Diana decidiu viver de forma modesta e poupar dinheiro."
+            ]
+
+            },
+            {
+                texto: "Diana deve gastar impulsivamente seu dinheiro com roupas e festas, negligenciando a importância da educação e das economias.",
+                afirmacao: ["Na juventude Diana foi imprudente, resultando em um currículo pobre e pouco chamativo.",
+                            "Na juventude, Diana gastou impulsivamente, acumulando dívidas significativas.",
+                            "Na juventude, Diana foi descuidada com suas finanças, gerando estresse e insegurança.",
+            ]
+            }
+        ]
+    },
+    {
+        enunciado: "Diana inicia sua carreira, qual a escolha ela deve tomar? ",
+        alternativas: [
+            {
+                texto: "Diana deve escolher viver acima das suas possibilidades, comprando um carro caro e morando em um apartamento luxuoso, financiando tudo com cartões de crédito.",
+                afirmacao: ["No inicio de sua carreia, ela gastou sem pensar nas consequências, se individando.",
+                            "No início de sua carreira, ela comprou um carro muito caro e se endividou.",
+                            "No início de sua carreira, ela fez escolhas imprudentes e se endividou.",
+
+            ]
+            },
+            {
+                texto: "Diana deve viver de forma modesta e economizar uma parte significativa de seu salário. Ela investe em uma carteira diversificada de ações e fundos, o que aumenta seu patrimônio ao longo do tempo, proporcionando segurança financeira.",
+                afirmacao: ["No início de sua carreira, ela escolheu economizar e investir sabiamente, aumentando o seu patrimônio.",
+                            "No início de sua carreira, ela viveu modestamente e poupou dinheiro",
+                            "No início da carreira, ela investiu em ações e funfos mútuos",
+            
+            ]
+                            
+            }
+        ]
+    },
+    {
+        enunciado: "Diana chega em sua meia-idade, e precisa se preparar para o futuro, qual a escolha que ela deve tomar?",
+        alternativas: [
+            {
+                texto: "Na meia-idade, Diana deve priorizar seu futuro financeiro. Ela contribui regularmente para um plano de aposentadoria e busca a orientação de um consultor financeiro para garantir que seus investimentos estejam alinhados com seus objetivos de longo prazo.",
+                afirmacao: ["Na meia-idade, Diana pensou em seu futuro, investindo em sua futura aposentadoria.",
+                            "Na meia-idade, Diana focou no futuro, aumentando suas contribuiçẽs para a aposentadoria.",
+                            "Na meia-idade, Diana priorizou seu futuro financeiro, economizando para a aposentadoria.",
+            ]
+            },
+            {
+                texto: "Diana pode optar por ignorar a importância da poupança para a aposentadoria, preferindo gastar seus recursos em viagens e bens de consumo de luxo.",
+                afirmacao: ["Na meia-idade, ela não deu importância a sua aposentadoria, ficando sem fundos para essa fase.",
+                            "Na meia-idade, ela negligenciou sua aposentadoria e ficou sem poupança para o futuro.",
+                            "Na meia-idade, ela não economizou para a aposentadoria, ficando financeiramente despreparada.",
+            ]
+
+            }
+        ]
+    },
+    {
+        enunciado: "Diana está cada vez mais perto de se aposentar, qual será a escolha mais prudente a se fazer? ",
+        alternativas: [
+            {
+                texto: "Perto da aposentadoria, Diana deve realizar um esforço consciente para pagar todas as suas dívidas. Ela quita o financiamento da casa e elimina quaisquer empréstimos pendentes.",
+                afirmacao: ["Perto de se aposentar, ela quitou todas as suas dívidas, se preaparando para uma velhice tranquila.",
+                            "Prestes a se aposentar, ela eliminou suas dívidas, preparando-se para um futuro seguro.",
+                            "Próxima da aposentadoria, ela quitou seus débitos, assegurando uma velhice serena.",
+                ]
+            },
+            {
+                texto: "Diana deve continuar a acumular dívidas, refinanciando sua casa para pagar outras despesas e contraindo novos empréstimos.",
+                afirmacao: ["Perto de se aposentar, ela se enterrou em dívidas, ficando sem nenhuma poupança para a aposentadoria.",
+                            "Próxima da aposentadoria, ela se endividou, deixando suas finanças em ruínas.",
+                            "Perto de se aposentar, ela contraiu muitas dívidas, acabando sem reservas para a aposentadoria.",
+            ]
+
+            }
+        ]
+    },
+    {
+        enunciado: "Diana finalmente começou a receber a sua aposentadoria, qual caminho ela deve seguir?",
+        alternativas: [
+            {
+                texto: "Diana deve gastar seu dinheiro sem um plano claro, retirando grandes quantias para viagens e luxos",
+                afirmacao: ["Já aposentada, Diana esgotou rapidamente suas economias, por não seguir um plano claro e eficiente.",
+                            "Aposentada, Diana gastou suas reservas com rapidez, por não ter seguido um plano financeiro eficiente.",
+                            "Após a aposentadoria, ela viu suas economias se esgotarem rapidamente, devido à falta de um plano claro.",
+            ]
+        
+            },
+            {
+                texto: "Já aposentada, Diana deve adotar um orçamento rigoroso para garantir que suas economias e investimentos durem pelo resto de sua vida. Ela prioriza gastos essenciais e desfruta de uma vida confortável, mas frugal, sem se preocupar em ficar sem dinheiro.",
+                afirmacao: ["Já aposentada, Diana fez escolhas sábias, garantindo uma vida confortável e estável.",
+                            "Na aposentadoria, Diana tomou decisões sábias, garantindo segurança e bem-estar.",
+                            "Aposentada, Diana seguiu um planejamento cuidadoso, assegurando uma vida confortável e segura.",
+            ]
+
+            }
+        ]
+    },
+];
--- script.js
+++ script.js
@@ -1,96 +1,12 @@
+import {aleatorio, nome} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-    
-    {
-        enunciado: "Durante a Juventude, Diana se depara com essas duas escolhas, qual é a mais prudente de se seguir? ",
-        alternativas: [
-            {
-                texto: "Diana deve investir em sua educação. Ela economiza dinheiro de trabalhos de meio período e bolsas de estudo para pagar a faculdade sem contrair muitas dívidas.",
-                afirmacao: [" Na juventude Diana fez cursos e construiu um currículo exemplar.",
-                            "Na juventude, Diana eonomizou sabiamente e evitou dívidas.",
-                            "Durante a juventude, Diana decidiu viver de forma modesta e poupar dinheiro."
-            ]
-
-            },
-            {
-                texto: "Diana deve gastar impulsivamente seu dinheiro com roupas e festas, negligenciando a importância da educação e das economias.",
-                afirmacao: ["Na juventude Diana foi imprudente, resultando em um currículo pobre e pouco chamativo.",
-                            "Na juventude, Diana gastou impulsivamente, acumulando dívidas significativas.",
-                            "Na juventude, Diana foi descuidada com suas finanças, gerando estresse e insegurança.",
-            ]
-            }
-        ]
-    },
-    {
-        enunciado: "Diana inicia sua carreira, qual a escolha ela deve tomar? ",
-        alternativas: [
-            {
-                texto: "Diana deve escolher viver acima das suas possibilidades, comprando um carro caro e morando em um apartamento luxuoso, financiando tudo com cartões de crédito.",
-                afirmacao: ["No inicio de sua carreia, ela gastou sem pensar nas consequências, se individando.",
-                            "No início de sua carreira, ela comprou um carro muito caro e se endividou.",
-                            "No início de sua carreira, ela fez escolhas imprudentes e se endividou.",
-
-            ]
-            },
-            {
-                texto: "Diana deve viver de forma modesta e economizar uma parte significativa de seu salário. Ela investe em uma carteira diversificada de ações e fundos, o que aumenta seu patrimônio ao longo do tempo, proporcionando segurança financeira.",
-                afirmacao: ["No início de sua carreira, ela escolheu economizar e investir sabiamente, aumentando o seu patrimônio.",
-                            "No início de sua carreira, ela viveu modestamente e poupou dinheiro",
-                            "No início da carreira, ela investiu em ações e funfos mútuos",
-            
-            ]
-                            
-            }
-        ]
-    },
-    {
-        enunciado: "Diana chega em sua meia-idade, e precisa se preparar para o futuro, qual a escolha que ela deve tomar?",
-        alternativas: [
-            {
-                texto: "Na meia-idade, Diana deve priorizar seu futuro financeiro. Ela contribui regularmente para um plano de aposentadoria e busca a orientação de um consultor financeiro para garantir que seus investimentos estejam alinhados com seus objetivos de longo prazo.",
-                afirmacao: ["Na meia-idade, Diana pensou em seu futuro, investindo em sua futura aposentadoria.",
-                            "Na meia-idade, Diana focou no futuro, aumentando suas contribuiçẽs para a aposentadoria.",
-                            "Na meia-idade, Diana priorizou seu futuro financeiro, economizando para a aposentadoria.",
-            ]
-            },
-            {
-                texto: "Diana pode optar por ignorar a importância da poupança para a aposentadoria, preferindo gastar seus recursos em viagens e bens de consumo de luxo.",
-                afirmacao: "Na meia-idade, ela não deu importância a sua aposentadoria, ficando sem fundos para essa fase. "
-            }
-        ]
-    },
-    {
-        enunciado: "Diana está cada vez mais perto de se aposentar, qual será a escolha mais prudente a se fazer? ",
-        alternativas: [
-            {
-                texto: "Perto da aposentadoria, Diana deve realizar um esforço consciente para pagar todas as suas dívidas. Ela quita o financiamento da casa e elimina quaisquer empréstimos pendentes.",
-                afirmacao: "Perto de se aposentar, Diana quitou todas as suas dívidas, se preaparando para uma velhice tranquila."
-            },
-            {
-                texto: "Diana deve continuar a acumular dívidas, refinanciando sua casa para pagar outras despesas e contraindo novos empréstimos.",
-                afirmacao: "Perto de se aposentar, Diana se enterrou em dívidas, ficando sem nenhuma poupança para a aposentadoria."
-            }
-        ]
-    },
-    {
-        enunciado: "Diana finalmente começou a receber a sua aposentadoria, qual caminho ela deve seguir?",
-        alternativas: [
-            {
-                texto: "Diana deve gastar seu dinheiro sem um plano claro, retirando grandes quantias para viagens e luxos",
-                afirmacao: "Já aposentada, ela esgotou rapidamente suas economias, por não seguir um plano claro e eficiente."
-            },
-            {
-                texto: "Já aposentada, Diana deve adotar um orçamento rigoroso para garantir que suas economias e investimentos durem pelo resto de sua vida. Ela prioriza gastos essenciais e desfruta de uma vida confortável, mas frugal, sem se preocupar em ficar sem dinheiro.",
-                afirmacao: " Já aposentada, ela fez escolhas sábias, garantindo uma vida confortável e estável."
-            }
-        ]
-    },
-];
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
 let atual = 0;
@@ -117,17 +33,39 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Baseado em suas escolhas, a vida de Diana foi assim:";
-    textoResultado.textContent = historiaFinal
+    caixaPerguntas.textContent = `Baseado em suas escolhas, a vida de ${nome} foi assim:`;
+    textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function substituiNome(){
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/Diana/g, nome);
+    }
+}
+
+function substituiNome_texto(){
+    for(const pergunta of perguntas){
+        pergunta.texto = pergunta.texto.replace(/Diana/g, nome);
+    }
+}
+
+substituiNome();
 mostraPergunta();
