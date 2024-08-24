699dcc6353ee04052fbe3a931e038eca17bfea1e
Mariana-zambon <88859896+Mariana-zambon@users.noreply.github.com>
2024-08-24 12:34:43 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -7,17 +7,16 @@ export const perguntas = [
                 texto:"Essa pessoa investir em sua educação. Ela economiza dinheiro de trabalhos de meio período e bolsas de estudo para pagar a faculdade sem contrair muitas dívidas.",
                 afirmacao: [" Na juventude essa pessoa fez cursos e construiu um currículo exemplar.",
                             "Na juventude, essa pessoa eonomizou sabiamente e evitou dívidas.",
-                            "Durante a juventude, essa pessoa decidiu viver de forma modesta e poupar dinheiro."
-                ]
-                proxima: 1;
+                            "Durante a juventude, essa pessoa decidiu viver de forma modesta e poupar dinheiro."],
+                proxima: 1,
 
             },
             {
                 texto: "Essa pessoa deve gastar impulsivamente seu dinheiro com roupas e festas, negligenciando a importância da educação e das economias.",
                 afirmacao: ["Na juventude  essa pessoa foi imprudente, resultando em um currículo pobre e pouco chamativo.",
                             "Na juventude, essa pessoa gastou impulsivamente, acumulando dívidas significativas.",
-                            "Na juventude, essa pessoa foi descuidada com suas finanças, gerando estresse e insegurança.",]
-                proxima: 2;            
+                            "Na juventude, essa pessoa foi descuidada com suas finanças, gerando estresse e insegurança.",],
+                proxima: 2,           
             
             }
         ]
@@ -27,64 +26,58 @@ export const perguntas = [
         enunciado: "Qual faculdade Diana deve escolher?",
         alternativas: [
             {
-                texto:"Essa pessoa deve escolher aquela qe mais combina com os seus interesses.",
+                texto:"Essa pessoa deve escolher aquela que mais combina com os seus interesses.",
                 afirmacao: [" Essa pessoa fez uma faculdade que se encaixava com os seus gostos.",
-                            "A faculdade que essa pessoa fez combinava com seus interesses.",
-
-                ]
-                proxima: 3;
+                            "A faculdade que essa pessoa fez combinava com seus interesses.",],
+                proxima: 3,
 
             },
             {
                 texto: "Essa pessoa deve escolher a faculdade que seus pais impõem.",
                 afirmacao: ["Essa pessoa fez um curso qye não se encaixava com os seus gostos",
-                            "A faculdade que essa pessoa fez não combinava com seus interesses.",
-            ]
-                proxima: 3;            
+                            "A faculdade que essa pessoa fez não combinava com seus interesses.",],
+                proxima: 3,            
             
             }
         ]
     },
 
     {
-        enunciado: "Diana foi a uma festa, ela deve:",
+        enunciado: "Diana foi a uma festa, deve:",
         alternativas: [
             {
                 texto:"Encher a cara e não lembrar de nada do que fez na festa.",
                 afirmacao: [" Essa pessoa foi a uma festa quando era jovem, e teve o pior porre da vida.",
-                            " Quando era jovem, foi a uma festa bebeu muito e passou mal.",
-
-                ]
-                proxima: 3;
+                            " Quando era jovem, foi a uma festa bebeu muito e passou mal.",],
+                proxima: 3,
 
             },
             {
                 texto: "Beber com sabedoria e curtir a festa com os amigos.",
                 afirmacao: [" Essa pessoa foi a uma festa quando era jovem, e se divertiu muito.",
-                            " Quando era jovem, foi a uma festa e fez memórias legais com seus amigos.",
-            ]
-                proxima: 3;            
+                            " Quando era jovem, foi a uma festa e fez memórias legais com seus amigos.",],
+                proxima: 3,            
             
             }
         ]
     },
 
     {
-        enunciado: "Diana inicia sua carreira, qual a escolha ela deve tomar? ",
+        enunciado: "Diana inicia sua carreira, qual escolha deve tomar? ",
         alternativas: [
             {
                 texto: "Essa pessoa deve escolher viver acima das suas possibilidades, comprando um carro caro e morando em um apartamento luxuoso, financiando tudo com cartões de crédito.",
                 afirmacao: ["No inicio de sua carreia, seu colega gastou sem pensar nas consequências, se individando.",
                             "No início de sua carreira, seu colega comprou um carro muito caro e se endividou.",
-                            "No início de sua carreira, seu colega fez escolhas imprudentes e se endividou.",]
-                proxima: 4;
+                            "No início de sua carreira, seu colega fez escolhas imprudentes e se endividou.",],
+                proxima: 4,
             },
             {
                 texto: "Essa pessoa deve viver de forma modesta e economizar uma parte significativa de seu salário. Ela investe em uma carteira diversificada de ações e fundos, o que aumenta seu patrimônio ao longo do tempo, proporcionando segurança financeira.",
                 afirmacao: ["No início de sua carreira, seu colega escolheu economizar e investir sabiamente, aumentando o seu patrimônio.",
                             "No início de sua carreira, seu colega viveu modestamente e poupou dinheiro",
-                            "No início da carreira, seu colega investiu em ações e funfos mútuos",]
-                proxima: 4;
+                            "No início da carreira, seu colega investiu em ações e funfos mútuos",],
+                proxima: 4,
                             
             }
         ]
@@ -96,15 +89,15 @@ export const perguntas = [
                 texto: "Na meia-idade, seu colega deve priorizar seu futuro financeiro. Ela contribui regularmente para um plano de aposentadoria e busca a orientação de um consultor financeiro para garantir que seus investimentos estejam alinhados com seus objetivos de longo prazo.",
                 afirmacao: ["Na meia-idade, pensou em seu futuro, investindo em sua futura aposentadoria.",
                             "Na meia-idade, focou no futuro, aumentando suas contribuiçẽs para a aposentadoria.",
-                            "Na meia-idade, priorizou seu futuro financeiro, economizando para a aposentadoria.",]
-                proxima: 5;
+                            "Na meia-idade, priorizou seu futuro financeiro, economizando para a aposentadoria.",],
+                proxima: 5,
             },
             {
                 texto: "Essa pessoa pode optar por ignorar a importância da poupança para a aposentadoria, preferindo gastar seus recursos em viagens e bens de consumo de luxo.",
                 afirmacao: ["Na meia-idade, não deu importância a sua aposentadoria, ficando sem fundos para essa fase.",
                             "Na meia-idade, negligenciou sua aposentadoria e ficou sem poupança para o futuro.",
-                            "Na meia-idade, não economizou para a aposentadoria, ficando financeiramente despreparada.",]
-                proxima: 6;
+                            "Na meia-idade, não economizou para a aposentadoria, ficando financeiramente despreparada.",],
+                proxima: 6,
 
             }
         ]
@@ -116,18 +109,15 @@ export const perguntas = [
             {
                 texto:" Ter filhos com a pessoa que ama.",
                 afirmacao: [" Teve três filhos filhos queridos.",
-                            " Teve dois filhos amorosos.",
-
-                ]
-                proxima: 7;
+                            " Teve dois filhos amorosos.",],
+                proxima: 7,
 
             },
             {
                 texto: "Não ter filhos e seguir apenas com o seu parceiro(a).",
                 afirmacao: [" Essa pessoa não teve filhos, dividiu sua vida apenas com o seu cônjuge.",
-                            " Não teve filhos, viveu uma vida feliz com o seu cônjuge. ",
-            ]
-                proxima: 7;            
+                            " Não teve filhos, viveu uma vida feliz com o seu cônjuge. ",],
+                proxima: 7,            
             
             }
         ]
@@ -139,18 +129,15 @@ export const perguntas = [
             {
                 texto:" Ter um cachorro salsicha chamado Bob",
                 afirmacao: [" Teve apenas um companheiro, um cachorro salsicha chamado Bob.",
-                            " Teve um cachorro salsicha chamado Bob.",
-
-                ]
-                proxima: 7;
+                            " Teve um cachorro salsicha chamado Bob.",],
+                proxima: 7,
 
             },
             {
                 texto: "Ter um gato amarelo chamado Morango.",
                 afirmacao: [" Teve apenas um companheiro, um gato laranja chamado Morango",
-                            " Teve um gato laranja chamado Morango",
-            ]
-                proxima: 7;            
+                            " Teve um gato laranja chamado Morango", ],
+                proxima: 7,            
             
             }
         ]
@@ -163,15 +150,15 @@ export const perguntas = [
                 texto: "Perto da aposentadoria, essa pessoa deve realizar um esforço consciente para pagar todas as suas dívidas. Ela quita o financiamento da casa e elimina quaisquer empréstimos pendentes.",
                 afirmacao: ["Perto de se aposentar, essa pessoa quitou todas as suas dívidas, se preaparando para uma velhice tranquila.",
                             "Prestes a se aposentar, essa pessoa eliminou suas dívidas, preparando-se para um futuro seguro.",
-                            "Próxima da aposentadoria, essa pessoa quitou seus débitos, assegurando uma velhice serena.",]
-                proxima: 8;
+                            "Próxima da aposentadoria, essa pessoa quitou seus débitos, assegurando uma velhice serena.",],
+                proxima: 8,
             },
             {
                 texto: "Essa pessoa deve continuar a acumular dívidas, refinanciando sua casa para pagar outras despesas e contraindo novos empréstimos.",
                 afirmacao: ["Perto de se aposentar, essa pessoa se enterrou em dívidas, ficando sem nenhuma poupança para a aposentadoria.",
                             "Próxima da aposentadoria, essa pessoa se endividou, deixando suas finanças em ruínas.",
-                            "Perto de se aposentar, essa pessoa contraiu muitas dívidas, acabando sem reservas para a aposentadoria.",]
-                proxima: 8;
+                            "Perto de se aposentar, essa pessoa contraiu muitas dívidas, acabando sem reservas para a aposentadoria.",],
+                proxima: 8,
             }
         ]
     },
