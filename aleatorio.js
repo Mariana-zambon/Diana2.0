342170c7a9ba530cde9b1363165aa8570047cccc
Mariana-zambon <88859896+Mariana-zambon@users.noreply.github.com>
2024-08-24 12:42:03 -0300

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,7 +0,0 @@
-const nomes= ["Mariana", "Stefany", "Kauê", "Laura", "Maria", "João Matheus", "João Gabriel", "Higor", "Igor", "Gabriel", "Rafael", "Vitor", "Heros", "Edmundo", "Rodrigo", "Gabriela", "Tamires"]
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes);
